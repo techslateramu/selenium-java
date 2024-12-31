5a7bf1cec0f07986786b0df28b741d07b0803669
Ramu Gurrapu <ramu@techslatelimited.com>
2024-12-31 23:20:53 +0530

    fixed annotations libs

--- src/test/java/com/example/GoogleTest.java
+++ src/test/java/com/example/GoogleTest.java
@@ -1,19 +1,23 @@
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
 
 public class GoogleTest {
     public WebDriver driver;
 
     @BeforeClass
     public void setUp() {
-        // Set Chrome options
+        // Set Chrome options for headless mode
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("--remote-allow-origins=*");
 
         // Set the path to the ChromeDriver
-        // System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
 
         driver = new ChromeDriver(options);
     }
