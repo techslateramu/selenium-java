af52b1e0c9e0fb97948a0f6f18cc4988b6176818
Ramu Gurrapu <ramu@techslatelimited.com>
2024-12-31 23:34:03 +0530

    added assertions

--- src/test/java/com/example/GoogleTest.java
+++ src/test/java/com/example/GoogleTest.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
@@ -16,16 +19,24 @@ public class GoogleTest {
         options.addArguments("--disable-gpu");
         options.addArguments("--remote-allow-origins=*");
 
-        // Set the path to the ChromeDriver
-        // System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-
         driver = new ChromeDriver(options);
     }
 
     @Test
     public void testGoogle() {
         driver.get("https://www.google.com");
-        // Your test code here
+
+        // Assertion to check the page title
+        String pageTitle = driver.getTitle();
+        Assert.assertEquals(pageTitle, "Google1", "Page title does not match!");
+
+        // // Assertion to check if the Google search button is present
+        // WebElement searchButton = driver.findElement(By.name("btnK"));
+        // Assert.assertTrue(searchButton.isDisplayed(), "Search button is not displayed on the page");
+
+        // // Additional assertion for Google's logo
+        // WebElement googleLogo = driver.findElement(By.id("hplogo"));
+        // Assert.assertTrue(googleLogo.isDisplayed(), "Google logo is not displayed on the page");
     }
 
     @AfterClass
